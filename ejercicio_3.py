45bce48c98e28aa88a81bd96c415efd3514c7636
Giagnacovo Ivan <93552640+giagnai@users.noreply.github.com>
2022-02-03 09:03:18 -0300

    Ejercicios de Practica finalizados.

---
+++ ejercicio_3.py
@@ -0,0 +1,46 @@
+# Matplotlib [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de matplotlib
+from turtle import color
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    print("Scatter Plot")
+
+    # NOTA: aproveche los ejemplos "scatter_plot" de clase
+
+    # Se desea graficar la función tanh para el siguiente
+    # intervalor de valores de "X"
+    x = np.arange(-np.pi, np.pi, 0.1)
+
+    # Función y = tanh(x) --> tangente hiperbólica
+    y = np.tanh(x)
+
+    # Alumno: Graficar la función utilizando "scatter"
+    # utilizando "x" e "y" ya disponible
+
+    # Colocar la leyenda y el label con el nombre de la función
+
+    # Elegir un marker a elección
+
+    # Crear acá su gráfico
+    fig = plt.figure()
+    fig.suptitle('Ejercicio N°3', fontsize=14)
+    ax = fig.add_subplot()
+
+    ax.scatter(x, y, color='green', marker='*', label='y=tanh(x)')
+    ax.set_facecolor('whitesmoke')
+    ax.grid('solid')
+    plt.show()
+
+    print("terminamos")
